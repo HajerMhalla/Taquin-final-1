00dbecff9f6828df5e07e8960adaa73be7faee2e
HajerMhalla <68785689+HajerMhalla@users.noreply.github.com>
2020-09-20 17:37:27 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include "Game.h"
+#include"definitions.h"
+#include <string>
+
+
+
+using namespace std;
+
+int main()
+{
+
+
+    Game g(SCREEN_WIDTH,SCREEN_HEIGHT,"taquin game");
+    return EXIT_SUCCESS;
+}
